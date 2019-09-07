ce700d238fd6c73790995429b41fbbe7688c5d0a
мeeт dadнanιa <meet.dadhania@outlook.com>
2019-09-07 11:25:20 +0530

    Lazy propogation

--- main.cpp
+++ main.cpp
@@ -1,35 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-class CP {
-public:
-  void solve(std::istream& in, std::ostream& out) { 
-    
-  }
-};
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    CP solver;
-    std::istream& in(std::cin);
-    std::ostream& out(std::cout);
-    solver.solve(in, out);
-    return 0;
+int main(){
+  cout << "Hello World!!";
+  return 0;
 }
-
-/*
-@ author : Meet Dadhania
-      ___                                 ___           ___           ___
-     /  /\        ___         ___        /__/\         /  /\         /__/|
-    /  /::\      /__/|       /  /\       \  \:\       /  /::\       |  |:|
-   /  /:/\:\    |  |:|      /  /:/        \__\:\     /  /:/\:\      |  |:|
-  /  /:/~/:/    |  |:|     /  /:/     ___ /  /::\   /  /:/  \:\   __|__|:|
- /__/:/ /:/   __|__|:|    /  /::\    /__/\  /:/\:\ /__/:/ \__\:\ /__/::::\____
- \  \:\/:/   /__/::::\   /__/:/\:\   \  \:\/:/__\/ \  \:\ /  /:/    ~\~~\::::/
-  \  \::/       ~\~~\:\  \__\/  \:\   \  \::/       \  \:\  /:/      |~~|:|~~
-   \  \:\         \  \:\      \  \:\   \  \:\        \  \:\/:/       |  |:|
-    \  \:\         \__\/       \__\/    \  \:\        \  \::/        |  |:|
-     \__\/                               \__\/         \__\/         |__|/
-
-*/
--- template/seg_tree.cpp
+++ template/seg_tree.cpp
@@ -1,47 +1,61 @@
-// Segment tree by MEET DADHANIA
+// Segment tree with LP by MEET DADHANIA
 #include<bits/stdc++.h>
 using namespace std;
 int build_tree(vector<int> &tree, int i){ // O(n)
-	if(tree[i] != -1) return tree[i];
-	else{
-		tree[i] = min(build_tree(tree, 2*i+1), build_tree(tree, 2*i+2));
-		return tree[i];
-	}
+  if(tree[i] != -1) return tree[i];
+  else{
+    tree[i] = min(build_tree(tree, 2*i), build_tree(tree, 2*i+1));
+    return tree[i];
+  }
 }
-int min_query(vector<int> &tree, int i, int li, int ri, int l, int r){ // O(log(n))
-	// Completely encompasses the range
-	if(li>=l && ri<=r) return tree[i];
-	// No Overlap
-	else if(r<li || l>ri) return INT_MAX;
-	// Partial Overlap
-	else return min( 
-		min_query(tree, 2*i+1, li, (li+ri)/2, l, r),
-		min_query(tree, 2*i+2, (li+ri)/2+1, ri, l, r));
+int min_query(vector<int> &tree, vector<int> &lazy_tree, int i, int li, int ri, int l, int r){ // O(log(n))
+  // Handle Lazy updations
+  if(lazy_tree[i] > 0){
+    cout << i << endl;
+    tree[i] += lazy_tree[i];
+    lazy_tree[2*i] += lazy_tree[i];
+    lazy_tree[2*i+1] += lazy_tree[i];
+    lazy_tree[i] = 0;
+  }
+  // Completely encompasses the range
+  if(li>=l && ri<=r) {return tree[i]; cout << i << endl;}
+  // No Overlap
+  else if(r<li || l>ri) return INT_MAX;
+  // Partial Overlap
+  else {
+    tree[i] = min( 
+    min_query(tree, lazy_tree, 2*i, li, (li+ri)/2, l, r),
+    min_query(tree, lazy_tree, 2*i+1, (li+ri)/2+1, ri, l, r));
+    return tree[i];
+  }
 }
-void update(vector<int> &tree, int index, int new_value, int n){ // O(log(n))
-	int i = n+index;
-	tree[i] = new_value;
-	while(i != 0){
-		int parent = -1;
-		if(i%2==0) parent = i/2-1;
-		else parent = i/2;
-		if(tree[parent] > tree[i]) tree[parent] = tree[i];
-		else break;
-		i = parent;
-	}
+// Add a number in range
+int range_update(vector<int> &tree, vector<int> &lazy_tree, int val, int i, int li, int ri, int l, int r){
+  if(li>=l && ri<=r){
+    lazy_tree[i] += val;
+    return tree[i] += val;
+  }
+  else if(r<li || l>ri) return INT_MAX;
+  else{
+    tree[i] = min(
+      range_update(tree, lazy_tree, val, 2*i, li, (li+ri)/2, l, r),
+      range_update(tree, lazy_tree, val, 2*i+1, (li+ri)/2+1, ri, l, r)
+      );
+    return tree[i];
+  }
 }
 int main(){
-	vector<int> arr = {5, 6, 2, 7, 9, 10, 1};
-	int n = arr.size();
-	
-	// Construction of Tree in TOP-DOWN
-	int len = n;
-	if(pow(2, (int)log2(n)) != n) len = pow(2,(int)log2(n)+1);
-	vector<int> tree(2*len-1, -1);
-	for(int i = len-1, j = 0 ; i < 2*len-1 ; i++, j++)
-		if(j >= n) tree[i] = INT_MAX;
-		else tree[i] = arr[j];
-	build_tree(tree, 0);
-	
-	return 0;
+  vector<int> arr = {5, 6, 2, 7, 9, 10, 1};
+  int n = arr.size();
+  
+  // Construction of Tree in TOP-DOWN
+  int len = n;
+  if(pow(2, (int)log2(n)) != n) len = pow(2,(int)log2(n)+1);
+  vector<int> tree(2*len, -1), lazy_tree(2*len, 0);
+  for(int i = len, j = 0 ; i < 2*len ; i++, j++)
+    if(j >= n) tree[i] = INT_MAX;
+    else tree[i] = arr[j];
+  build_tree(tree, 1);
+  
+  return 0;
 }
