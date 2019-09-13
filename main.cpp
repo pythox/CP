c9dc8de52b61995fc0716c6d7faf3db53ee20f53
pythox <pythox@LAPTOP-DNNBCA2L.localdomain>
2019-09-13 15:28:53 +0530

    using fugitive

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
-// Tarjan's Algorithm for strongly connected components
+// Kosaraju Algorithm for strongly connected components
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-  
+  int n,e;
+  cin >> n >> e;
+  vector<vector<pair<int, int>>> graph(n);
+  for(int i=0 ; i<e ; i++){
+    int v1, v2, w;
+    cin >> v1 >> v2 >> w;
+    graph[v1].push_back(make_pair(v1, w));
+  } 
   return 0;
 } 
